d080e0f490b6f659bbcdd514dde8100e51dd9dd9
Krisna Fathurahman <krisna.fathurahman@gmail.com>
2021-08-04 15:41:21 +0700

    setup(typeorm): try using inject connection instead of repository

--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { InjectConnection } from '@nestjs/typeorm';
+import { Connection, Repository } from 'typeorm';
 import { LegacyTest, Test } from './data.entity';
 
 @Injectable()
 export class DataService {
+  private readonly legacyTestRepository: Repository<LegacyTest>;
+  private readonly testRepository: Repository<Test>;
+
   constructor(
-    @InjectRepository(Test)
-    private readonly testRepository: Repository<Test>,
-    @InjectRepository(LegacyTest, 'legacyConnection')
-    private readonly legacyTestRepository: Repository<LegacyTest>,
-  ) {}
+    @InjectConnection()
+    private readonly connection: Connection,
+    @InjectConnection('legacyConnection')
+    private readonly legacyConnection: Connection,
+  ) {
+    this.testRepository = this.connection.getRepository(Test);
+    this.legacyTestRepository = this.legacyConnection.getRepository(LegacyTest);
+  }
 
   async getData() {
     await this.testRepository.find();
